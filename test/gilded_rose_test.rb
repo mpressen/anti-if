091e41b01bc8d8c7d3b1e38a1820e301b6e6d922
mpressen <maximilien.pressense@hey.com>
2023-11-14 14:47:08 +0100

    green tests on minitest

---
+++ test/gilded_rose_test.rb
@@ -0,0 +1,12 @@
+require File.join(File.dirname(__FILE__), '../gilded_rose')
+require 'minitest/autorun'
+
+class GildedRoseTest < Minitest::Test
+
+  def test_foo
+    items = [Item.new("foo", 0, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].name, "foo"
+  end
+
+end
