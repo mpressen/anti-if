281663142dd4244ec51ab15eb19bde5674c68735
mpressen <maximilien.pressense@hey.com>
2023-11-30 12:05:26 +0100

    100% mutation covered

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,49 +4,40 @@ class GildedRose
     @items = items
   end
 
+  def increase_quality(item)
+    item.quality += 1 if item.quality < 50
+  end
+
+  def degrade_quality(item)
+    item.quality -= 1 if item.quality > 0
+  end
+
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+      if item.name == "Aged Brie"
+        item.sell_in -= 1
+        increase_quality(item)
+        if item.sell_in < 0
+          increase_quality(item)
         end
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        item.sell_in -= 1
+        increase_quality(item)
+        if item.sell_in < 10
+          increase_quality(item)
+        end
+        if item.sell_in < 5
+          increase_quality(item)
+        end
+        if item.sell_in < 0
+          item.quality = 0
+        end
+      elsif item.name == "Sulfuras, Hand of Ragnaros"
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+        item.sell_in -= 1
+        degrade_quality(item)
+        if item.sell_in < 0
+          degrade_quality(item)
         end
       end
     end
